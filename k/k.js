d40aea1b8e78d135f07a5c0e241aa4ca490982e9
huhuh1234567 <huhuh1234567@126.com>
2018-02-06 18:37:07 +0800

    add list

--- k/k.js
+++ k/k.js
@@ -1,5 +1,158 @@
 (function(){
 
+	function ListNode(value){
+		this._ = value;
+		this.next$ = null;
+		this.previous$ = null;
+	}
+
+	function List(){
+		this.length = 0;
+		this.head$ = null;
+		this.tail$ = null;
+	}
+
+	List.$ = function(value){
+		return new ListNode(value);
+	};
+
+	List.$$ = function(arr){
+		var rst = new List();
+		for(var i=0; i<arr.length; i++){
+			List.push(arr[i]);
+		}
+		return rst;
+	};
+
+	List.isBefore$ = function(l$,r$){
+		for(; r$&&r$!==l$; r$ = r$.next$){
+		}
+		return r$===null;
+	};
+
+	List.prototype = {
+		constructor: List,
+		insert$: function(target$,next$){
+			var previous$ = next$!==null?next$.previous$:this.tail$;
+			target$.next$ = next$;
+			target$.previous$ = previous$;
+			if(previous$){
+				previous$.next$ = target$
+			}
+			else{
+				this.head$ = target$
+			}
+			if(next$){
+				next$.previous$ = target$;
+			}
+			else{
+				this.tail$ = target$;
+			}
+			this.length++;
+			return target$;
+		},
+		remove$: function(target$){
+			var next$ = target$.next$;
+			var previous$ = target$.previous$;
+			if(previous$){
+				previous$.next$ = next$;
+			}
+			else{
+				this.head$ = next$;
+			}
+			if(next$){
+				next$.previous$ = previous$;
+			}
+			else{
+				this.tail$ = previous$;
+			}
+			this.length--;
+			return next$;
+		},
+		push: function(value){
+			return this.insert$(List.$(value),null);
+		},
+		pop = function(list){
+			var rst = this.tail$._;
+			this.remove$(this.tail$);
+			return rst;
+		},
+		unshift: function(list,value){
+			return list.insert$(List.$(value),list.head$);
+		},
+		shift: function(list){
+			var rst = this.head$._;
+			this.remove$(this.head$);
+			return rst;
+		},
+		_: function(){
+			var list = this;
+			var end = false;
+			var last$ = null;
+			return {
+				remove: function(){
+					if(last$!=null){
+						var next$ = list.remove$(last$);
+						last$ = next$===null?list.tail$:next$.previous$;
+					}
+				},
+				next: function(){
+					if(!end){
+						var v = undefined;
+						last$ = last$===null?list.head$:last$.next$;
+						if(last$!==null){
+							v = last$._;
+						}
+						else{
+							end = true;
+						}
+						return v;
+					}
+					else{
+						return undefined;
+					}
+				}
+			}
+		},
+		__: function(){
+			var rst = new Array(this.length);
+			var current$ = this.head$;
+			for(var i=0; i<this.length; i++){
+				rst[i] = current$._;
+				current$=current$.next$
+			}
+			return rst;
+		},
+		sort: function(less){
+			var current$ = this.tail$;
+			while(current$!==null){
+				var next$ = current$.next$;
+				var previous$ = current$.previous$;
+				this.remove$(current$);
+				while(next$!==null){
+					if(less(next$._,current$._)){
+						next$ = next$.next$;
+					}
+					else{
+						break;
+					}
+				}
+				this.insert$(current$,next$);
+				current$ = previous$;
+			}
+		},
+		pushAll: function(list){
+			for(var current$=list.head$; current$!==null; current$=current$.next$){
+				this.push(current$._);
+			}
+		},
+		unshiftAll: function(list){
+			for(var current$=list.tail$; current$!==null; current$=current$.previous$){
+				this.unshift(current$._);
+			}
+		}
+	};
+
 	function foreach_(itor,block){
 		var end = false;
 		var v = undefined;
